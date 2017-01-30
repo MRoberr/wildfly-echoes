50bd310689a1247faedab5e0b4486f908a9dcfce
MRoberr <majairober@gmail.com>
2017-01-30 17:07:12 +0200

    init commit

---
+++ wildfly-echos-common/src/main/java/rober/wildfly/echos/common/IEcho.java
@@ -0,0 +1,13 @@
+package rober.wildfly.echos.common;
+
+import java.util.List;
+
+import rober.wildfly.echos.jpa.Echo;
+
+public interface IEcho {
+
+	public int storeEcho(String message);
+	public Echo getEchoById(int p_id);
+	public List<Echo> getAllEchos();
+	
+}
